fbb8ed1cf6450637a9d96a89f68364d92ca4fc79
Javier Ramos <javier.ramos@applydigital.com>
2024-10-15 15:35:33 -0500

    feat: not use env var

--- src/services/games.ts
+++ src/services/games.ts
@@ -1,5 +1,5 @@
 export async function getGames({genre = 'all', page= 1}: {genre?: string, page?: number}): Promise<{games: any[], availableFilters: string[]}> {
-    const api = `http://localhost:3000/api/games?genre=${genre}&page=${page}`
+    const api = `/api/games?genre=${genre}&page=${page}`
     const searchParams = new URLSearchParams({genre, page: `${page}`})
     const products = await fetch(api);
     return await products.json();
